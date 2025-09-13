11b6304029c4ce05b4ee45669a921d1e98eb52c4
stevefearson <95326589+stevefearson@users.noreply.github.com>
2025-09-12 23:15:14 -0700

    Update pdt.js

--- pages/api/pdt.js
+++ pages/api/pdt.js
@@ -3,6 +3,11 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === "OPTIONS") {
+    res.status(200).end(); // Respond to preflight
+    return;
+  }
+
   const { tx } = req.query;
   const authToken = "JNpQRWAPwNn4sPt3shhD8ABgHc8OaZXyYTokU1-0L0tmIMaQYktsT6NhVTq"; // Replace with your actual sandbox PDT token
 
